84f53d5e5f62b23c12acfffdce0279315879ab09
小松大二 <@LAPTOP-OMBU047L>
2023-07-24 14:26:25 +0900

    修正完了

--- first_traning/src/change/Problem04.java
+++ first_traning/src/change/Problem04.java
@@ -36,7 +36,6 @@ public class Problem04 {
          *
          */
 
-
     }
 
     /**
@@ -57,104 +56,93 @@ public class Problem04 {
         List<Integer> dataList = NumericUtil.makeRandomList(indexCount, maxNumber);
 
         // 作成したランダム値のリストを国情報リストに変換する
-        List<Country> retList =
-                dataList.parallelStream() // 並列で処理
-                        .map(r -> Country.getCountry(r)) // 数値を国番号として国情報を取得
-                        .collect(Collectors.toList());   // 取得したものをリストに格納
+        List<Country> retList = dataList.parallelStream() // 並列で処理
+                .map(r -> Country.getCountry(r)) // 数値を国番号として国情報を取得
+                .collect(Collectors.toList()); // 取得したものをリストに格納
 
         return retList;
     }
 
-
     private static void aggregateNumber(List<Country> countryList) {
         // ※ 判定にif文ではなく、switch文を使用すること
 
-        int usaPopulation = 0;
         int japanPopulation = 0;
-        int italyPopulation = 0;
-        int indiaPopulation = 0;
-        int ausPopulation = 0;
-        int fraPopulation = 0;
+        int usaPopulation = 0;
         int chinaPopulation = 0;
+        int indiaPopulation = 0;
         int ukPopulation = 0;
+        int italyPopulation = 0;
+        int fraPopulation = 0;
+        int ausPopulation = 0;
         int korPopulation = 0;
         int canPopulation = 0;
         int deuPopulation = 0;
 
-
         for (Country country : countryList) {
 
-        switch (country) {
-
-            case USA:
-            usaPopulation++;
-            break;
+            switch (country) {
 
             case JPN:
-            japanPopulation++;
-            break;
+                japanPopulation++;
+                break;
 
-            case ITR:
-            italyPopulation++;
-            break;
-
-            case IND:
-            indiaPopulation++;
-            break;
-
-            case AUS:
-            ausPopulation++;
-            break;
-
-            case FRA:
-            fraPopulation++;
-            break;
+            case USA:
+                usaPopulation++;
+                break;
 
             case CHN:
-            chinaPopulation++;
-            break;
+                chinaPopulation++;
+                break;
+
+            case IND:
+                indiaPopulation++;
+                break;
 
             case GBR:
-            ukPopulation++;
-            break;
+                ukPopulation++;
+                break;
+
+            case ITR:
+                italyPopulation++;
+                break;
+
+            case FRA:
+                fraPopulation++;
+                break;
+
+            case AUS:
+                ausPopulation++;
+                break;
 
             case KOR:
-            korPopulation++;
-            break;
+                korPopulation++;
+                break;
 
             case CAN:
-            canPopulation++;
-            break;
+                canPopulation++;
+                break;
 
             case DEU:
-            deuPopulation++;
-            break;
+                deuPopulation++;
+                break;
 
+            default:
+                //未定義の国にはなにもしない
+                break;
 
-
-
-
-        default:
-            //未定義の国にはなにもしない
-            break;
+            }
 
         }
-
-        System.out.println("usa: " + usaPopulation);
         System.out.println("japan: " + japanPopulation);
-        System.out.println("italy:" + italyPopulation);
-        System.out.println("india: " + indiaPopulation);
-        System.out.println("aus: " + ausPopulation);
-        System.out.println("fra: " + fraPopulation);
+        System.out.println("usa: " + usaPopulation);
         System.out.println("china: " + chinaPopulation);
+        System.out.println("india: " + indiaPopulation);
         System.out.println("uk: " + ukPopulation);
-        System.out.println("korea" + korPopulation);
-        System.out.println("canada" + canPopulation);
-        System.out.println("germany" + deuPopulation);
-
-
-
-
-        }
+        System.out.println("italy:" + italyPopulation);
+        System.out.println("fra: " + fraPopulation);
+        System.out.println("aus: " + ausPopulation);
+        System.out.println("korea: " + korPopulation);
+        System.out.println("canada: " + canPopulation);
+        System.out.println("germany: " + deuPopulation);
     }
 }
